2e1a9c30c69873f67d207a4698d8fd17da90251a
DNNMonster <coryg@storeoborealis.com>
2020-03-27 14:09:49 -0700

    Add verbose logging toggle

--- Components/ProviderUtils.cs
+++ Components/ProviderUtils.cs
@@ -15,22 +15,26 @@ namespace OS_Square
     {
         readonly static Square.Connect.Client.Configuration _config;
         readonly static string _locationId;
+        private readonly static Boolean _verboseLogging = false;
+        private static readonly EventLogController _objEventLog;
 
         static ProviderUtils() {
             var settings = ProviderUtils.GetProviderSettings();
             var accessToken = NBrightCore.common.Security.Decrypt(PortalController.Instance.GetCurrentPortalSettings().GUID.ToString(), settings.GetXmlProperty("genxml/textbox/accesstoken"));
             var url = settings.GetXmlPropertyBool("genxml/checkbox/sandboxmode") == true ? "https://connect.squareupsandbox.com" : "https://connect.squareup.com";
+            var debugmode = settings.GetXmlPropertyBool("genxml/checkbox/debugmode");
 
             _config = new Square.Connect.Client.Configuration(new Square.Connect.Client.ApiClient(url)){ AccessToken = accessToken };
 
             // Get the default location or an exact name match as specified in the plugin settings
             var squareLocationName = settings.GetXmlProperty("genxml/textbox/locationname");
-
-            var objEventLog = new EventLogController();
-            objEventLog.AddLog("OS_Square Message", "Location Name : " + squareLocationName, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
-            objEventLog.AddLog("OS_Square Message", "Url : " + url, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
-            objEventLog.AddLog("OS_Square accessToken", "accessToken : " + accessToken, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
-
+            _objEventLog = new EventLogController();
+            if (_verboseLogging) {
+                //var objEventLog = new EventLogController();
+                _objEventLog.AddLog("OS_Square Message", "Location Name : " + squareLocationName, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
+                _objEventLog.AddLog("OS_Square Message", "Url : " + url, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
+                _objEventLog.AddLog("OS_Square accessToken", "accessToken : " + accessToken, PortalController.Instance.GetCurrentPortalSettings(), UserController.Instance.GetCurrentUserInfo().UserID, EventLogController.EventLogType.ADMIN_ALERT);
+            }
 
             _locationId = GetLocationId(squareLocationName);
             //_locationId = "7D721KQTNCYWF";
@@ -45,7 +49,8 @@ namespace OS_Square
         public static CreatePaymentResponse GetChargeResponse(OrderData orderData, string nonce)
         {
             var settings = ProviderUtils.GetProviderSettings();
-
+            var portalSettings = PortalController.Instance.GetCurrentPortalSettings();
+            var userId = UserController.Instance.GetCurrentUserInfo().UserID;
             // Every payment you process must have a unique idempotency key.
             // If you're unsure whether a particular payment succeeded, you can reattempt
             // it with the same idempotency key without worrying about double charging
@@ -73,16 +78,11 @@ namespace OS_Square
             var storename = StoreSettings.Current.SettingsInfo.GetXmlProperty("genxml/textbox/storename");
             var note = storename + " Order Number: " + orderData.OrderNumber;
 
-
-            var objEventLog = new EventLogController();
-            var portalSettings = PortalController.Instance.GetCurrentPortalSettings();
-            var userId = UserController.Instance.GetCurrentUserInfo().UserID;
-            objEventLog.AddLog("OS_Square message", "ApiKey : " + _config.Password, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
-            objEventLog.AddLog("OS_Square message", "AccessToken : " + _config.AccessToken, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
-            objEventLog.AddLog("OS_Square message", "Header : " + Newtonsoft.Json.JsonConvert.SerializeObject(_config.ApiClient.Configuration.DefaultHeader), portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
-
-            
-           
+            if (_verboseLogging) {
+                _objEventLog.AddLog("OS_Square message", "ApiKey : " + _config.Password, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
+                _objEventLog.AddLog("OS_Square message", "AccessToken : " + _config.AccessToken, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
+                _objEventLog.AddLog("OS_Square message", "Header : " + Newtonsoft.Json.JsonConvert.SerializeObject(_config.ApiClient.Configuration.DefaultHeader), portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
+            }
 
             // Creating Payment Request
             // NOTE: OS Order Id is passed in both the reference_id & note field
@@ -97,8 +97,7 @@ namespace OS_Square
             }
             catch (Exception ex)
             {
-                objEventLog.AddLog("OS_Square err ", "Message : " + ex.Message, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
-
+                _objEventLog.AddLog("OS_Square err ", "Message : " + ex.Message, portalSettings, userId, EventLogController.EventLogType.ADMIN_ALERT);
                 orderData.AddAuditMessage(ex.Message, "notes", UserController.Instance.GetCurrentUserInfo().Username, "False");
                 //throw;
             }
