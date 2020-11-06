4c4a90158ecc6637616797a7c23534ebe6e80c04
DNNMonster <coryg@storeoborealis.com>
2020-11-05 17:00:20 -0800

    Remove old comments

--- Return.aspx.cs
+++ Return.aspx.cs
@@ -1,15 +1,3 @@
-// --- Copyright (c) notice NevoWeb ---
-//  Copyright (c) 2014 SARL NevoWeb.  www.nevoweb.com. The MIT License (MIT).
-// Author: D.C.Lee
-// ------------------------------------------------------------------------
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-// ------------------------------------------------------------------------
-// This copyright notice may NOT be removed, obscured or modified without written consent from the author.
-// --- End copyright notice --- 
 
 using System;
 using System.Collections.Generic;
