3d9b50c4b0e8fe52e4bac7d918deffa139264c43
DNNMonster <coryg@storeoborealis.com>
2021-04-22 21:13:08 -0700

    Bump to version 3.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("i502 Club")]
 [assembly: AssemblyProduct("OS_Square")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("046f4169-8d4e-44f9-99fa-2250be352a70")]
 
-[assembly: AssemblyVersion("3.0.2.*")]
+[assembly: AssemblyVersion("3.1.0.*")]
